00e98709a381a981bfba3b8219235d894e81b8ac
flashnea <blt_raph@yahoo.com>
2025-10-06 19:12:42 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white shadow-inner p-4 text-center text-gray-600">
+      &copy; 2025 Mon Atelier 3D. Tous droits réservés.
+    </footer>
+  );
+}
